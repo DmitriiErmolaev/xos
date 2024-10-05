bb44738566f6ddcc8ff6dbf787108345ce7df40c
Dmitrii Ermolaev <bugiman532@gmail.com>
2024-10-05 11:50:09 +0300

    release-version 1.0

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/index.scss';
-import { RouterProvider } from 'react-router-dom';
 import Game from './pages/Game/components/Game';
-import routes from './router/routes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <RouterProvider router={routes}/>
-    {/* <Game /> */}
+    <Game />
   </React.StrictMode>
 );
